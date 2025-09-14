22098fccffae3e8f1d01d4c4dd12184fbbc92abd
mbed101 <mahend101@protonmail.com>
2025-09-14 22:57:46 +0200

    added pdfsharp

--- pdfcutter/pdfcutter/MainForm.Designer.cs
+++ pdfcutter/pdfcutter/MainForm.Designer.cs
@@ -29,17 +29,29 @@
         private void InitializeComponent()
         {
             openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
             SuspendLayout();
             // 
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button1
+            // 
+            button1.Location = new Point(293, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 34);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "MainForm";
             Text = "Pdfcutter";
             ResumeLayout(false);
@@ -48,5 +60,6 @@
         #endregion
 
         private OpenFileDialog openFileDialog1;
+        private Button button1;
     }
 }
--- pdfcutter/pdfcutter/MainForm.cs
+++ pdfcutter/pdfcutter/MainForm.cs
@@ -1,3 +1,8 @@
+using System.IO;
+using System.Text;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
 namespace pdfcutter
 {
     public partial class MainForm : Form
@@ -5,6 +10,35 @@ namespace pdfcutter
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Load pdf file 
+            var path = @"D:\BOOK.pdf";
+
+            if (System.IO.File.Exists(path))
+            {
+                MessageBox.Show("Exists");
+            }
+
+            PdfDocument inputDocument=PdfReader.Open(path, PdfDocumentOpenMode.Import);
+            int pageCount = inputDocument.PageCount;
+
+            Console.WriteLine($"Splitting pdf with {pageCount} pages");
+
+            var extractcount = 10;
+
+            PdfDocument outputDocument = new PdfDocument();
+
+            for (int i = 0; i < extractcount; i++)
+            {
+
+                PdfPage page = inputDocument.Pages[i];
+                outputDocument.AddPage(page);
+            }
+
+            outputDocument.Save(@"D:\BOOK_new.pdf");
         }
     }
 }
